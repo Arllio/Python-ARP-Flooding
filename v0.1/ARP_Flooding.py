f22280a6147aa6bc0ec852628b1e11aec1cc3063
agus makmun <summon.agus@gmail.com>
2015-06-24 03:17:50 +0700

    Update ARP_Flooding.py

--- v0.1/ARP_Flooding.py
+++ v0.1/ARP_Flooding.py
@@ -3,7 +3,7 @@ Name             : Python ARP Flooding
 Created By       : Agus Makmun (Summon Agus)
 Blog             : bloggersmart.net - python.web.id
 License          : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
-Documentation    : https://github.com/agusmakmun/Some-Examples-of-Simple-Python-Script/
+Documentation    : https://github.com/agusmakmun/Python-ARP-Flooding/
 Thanks to        : RR12 - Indonesian Backtrack Team
 """
 
